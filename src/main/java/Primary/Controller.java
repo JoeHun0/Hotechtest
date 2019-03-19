e842180ec22ed91b5e59ef4d939f43dc104a52b3
Kotnyek József <joehun0@gmail.con>
2019-03-19 11:40:50 +0100

    Initial commit

---
+++ src/main/java/Primary/Controller.java
@@ -0,0 +1,9 @@
+package Primary;
+
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public void onMouseClick(MouseEvent mouseEvent) {
+        System.out.println("akármi");
+    }
+}
